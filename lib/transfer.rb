eca083838d5229a5898a1300546c694eb1d2b031
Brandon Marrero <brandonmar97@gmail.com>
2020-03-30 23:29:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,9 @@ class Transfer
       @receiver.balance += @amount
       @status = 'complete'
     else 
-      "Transaction rejected. Please check your account balance."
       @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+      
     end
   end 
 end
